2338b58f86fee5f3b360c25521cfb96cd69ef9de
Amrender9798 <amrender9798@gmail.com>
2023-07-31 21:57:17 +0100

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,10 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+
+        //This is code of second commit
+        for(int j = 1; j <= 5; j++){
+            System.out.println("i = " + i);
+        }
     }
 }
